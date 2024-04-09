f436bfca9dabcac4fcf099ff5ecd831c43ce416e
Guillaume Charbonnier <guillaume.charbonnier@araymond.com>
2024-04-09 02:51:06 +0200

    feat: add openapi documentation to the application

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { Logger } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+
 import { dataSource } from "../config/dataSource";
 import { AppModule } from "./app.module";
 
@@ -11,7 +13,18 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ["error", "warn", "log"],
   });
+
+  const openapiConfig = new DocumentBuilder()
+    .setTitle("Food Carbon Emission API")
+    .setDescription("The food carbon emission API")
+    .setVersion("0.1.0")
+    .build();
+
+  const document = SwaggerModule.createDocument(app, openapiConfig);
+  SwaggerModule.setup("docs", app, document);
+
   await app.listen(3000);
 }
+
 Logger.log(`Server running on http://localhost:3000`, "Bootstrap");
 bootstrap();
